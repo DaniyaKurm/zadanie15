4dee78b83b16b5664748221a269251374e828a82
Your NameDaniya <you@example.com>
2023-12-25 21:35:06 +0300

    Initial commit

--- src/test/java/ru/netology/web/BankCardTest.java
+++ src/test/java/ru/netology/web/BankCardTest.java
@@ -1,5 +1,6 @@
 package ru.netology.web;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,7 +17,7 @@ public class BankCardTest {
 
     @BeforeAll
     public static void setupAll() {
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+
         WebDriverManager.chromedriver().setup();
     }
 
